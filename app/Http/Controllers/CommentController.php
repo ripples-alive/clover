a7772cc650240f0b6875e170139ad6a0b12d4c82
JayvicWen <564311873@qq.com>
2015-03-09 10:13:04 +0800

    Add and list comment.

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,6 +8,14 @@
 
 namespace Clover\Http\Controllers;
 
+use Request;
+use Validator;
+
+use UserAuth;
+use Clover\Exceptions\NotFoundException;
+use Clover\Exceptions\InputException;
+use Clover\Models\Comment;
+
 class CommentController extends Controller {
 
     public function __construct()
@@ -15,14 +23,54 @@ class CommentController extends Controller {
         $this->middleware('userFilter');
     }
 
+    /**
+     * @topic_id
+     */
     public function getList()
     {
-        // TODO
+        // TODO: add more parameters
+        $comments = Comment::where('topic_id', intval(Request::input('topic_id')))->orderBy('created_at')->get();
+
+        return [
+            'message' => '列出评论成功',
+            'comments' => $comments,
+        ];
     }
 
+    /**
+     * @topic_id
+     * @[reply_to]
+     * @content
+     */
     public function postAdd()
     {
-        // TODO
+        $comment = new Comment();
+        $comment->author = UserAuth::id();
+
+        $comment->topic_id = intval(Request::input('topic_id'));
+        if (Validator::make(['key' => $comment->topic_id], ['key' => 'exists:topic,id'])->fails()) {
+            throw new NotFoundException('话题不存在');
+        }
+
+        $reply_to = Request::input('reply_to');
+        if (!empty($reply_to)) {
+            $comment->reply_to = intval($reply_to);
+            if (Validator::make(['key' => $comment->reply_to], ['key' => 'exists:comment,id'])->fails()) {
+                throw new NotFoundException('回复的评论不存在');
+            }
+        }
+
+        $comment->content = Request::input('content');
+        if (empty($comment->content)) {
+            throw new InputException('评论内容不得为空');
+        }
+
+        $comment->save();
+
+        return [
+            'message' => '评论成功',
+            'comment' => $comment,
+        ];
     }
 
 } 
